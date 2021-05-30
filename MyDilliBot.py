b9082b03b4cc301f78c6d9ff469b3bb436a0d1b9
Saloni Kalra <salonikalra1999@gmail.com>
2021-05-30 18:02:46 +0530

    Pings telegram server every 0.5 secs for NEW updates using the offset variable in get url. Keeps track of latest update id.

--- MyDilliBot.py
+++ MyDilliBot.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import time
+import time    
 
 def main():
 
@@ -9,27 +9,35 @@ def main():
     groupToId = ""
     urlFrom = "https://api.telegram.org/bot"+botId+"/getUpdates"
     urlTo = "https://api.telegram.org/bot"+botId+"/sendMessage?chat_id=-"+groupToId+"&text="
-
-    last_message = None
+    lastUpdateId = None
+    
     while True:
-
-        response = requests.get(urlFrom)
+        if lastUpdateId:
+            response = requests.get(urlFrom+"?offset="+str(lastUpdateId))
+        else:
+            response = requests.get(urlFrom)
         # print (response)
         content = response.content.decode("utf8")
         # print (content)
         updates = json.loads(content)
-        # print (updates)
-        num_updates = len(updates["result"])
-        # print (num_updates)
-        last_update = num_updates - 1
-        message = updates["result"][last_update]["message"]["text"]
-        # print (message)
+        print (updates)
 
-        if (message) != last_message:
+        if len(updates["result"]) > 0:
+            num_updates = len(updates["result"])
+            # print (num_updates)
+            last_update = num_updates - 1
+            message = updates["result"][last_update]["message"]["text"]
+            # print (message)
+
             if "delhi" in message.lower():
                 response = requests.post(urlTo+message)
                 # print (response)
-            last_message = message
+
+            updateIds = []
+            for update in updates["result"]:
+                updateIds.append(int(update["update_id"]))
+            lastUpdateId = max(updateIds)
+            lastUpdateId = lastUpdateId + 1
         time.sleep(0.5)
 
 if __name__ == '__main__':
