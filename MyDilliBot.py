53bef21b0a7fa324254888c7daf6e73d36bd0d7e
Saloni Kalra <salonikalra1999@gmail.com>
2021-05-30 18:12:36 +0530

    Catch exceptions when the message is an image and doesn't have text key, etc.

--- MyDilliBot.py
+++ MyDilliBot.py
@@ -6,7 +6,6 @@ import urllib
 def main():
 
     botId = ""
-    groupFromId = ""
     groupToId = ""
     urlFrom = "https://api.telegram.org/bot"+botId+"/getUpdates?timeout=100"
     urlTo = "https://api.telegram.org/bot"+botId+"/sendMessage?chat_id=-"+groupToId+"&text="
@@ -23,27 +22,31 @@ def main():
         updates = json.loads(content)
         # print (updates)
 
-        if len(updates["result"]) > 0:
-            num_updates = len(updates["result"])
-            # print (num_updates)
-            last_update = num_updates - 1
+        if(updates.get("result")):
+            if len(updates["result"]) > 0:
+                num_updates = len(updates["result"])
+                # print (num_updates)
+                last_update = num_updates - 1
 
-            chatId = updates["result"][last_update]["message"]["chat"]["id"]
-            if str(chatId) == "-":
-                message = updates["result"][last_update]["message"]["text"]
-                # print (message)
-                
-                if "delhi" in message.lower():
-                    message = urllib.parse.quote_plus(message)
-                    response = requests.post(urlTo+message)
-                    # print (response)
+                if (updates["result"][last_update].get("message")):
+                    if (updates["result"][last_update]["message"]["chat"].get("id")):
+                        chatId = updates["result"][last_update]["message"]["chat"]["id"]
+                        if str(chatId) == "-" or str(chatId) == "-" or str(chatId) == "-":
 
-            updateIds = []
-            for update in updates["result"]:
-                updateIds.append(int(update["update_id"]))
-            lastUpdateId = max(updateIds)
-            lastUpdateId = lastUpdateId + 1
-        time.sleep(0.5)
+                            if (updates["result"][last_update]["message"].get("text")):
+                                message = updates["result"][last_update]["message"]["text"]
+                                # print (message)
+                                if "delhi" in message.lower():
+                                    message = urllib.parse.quote_plus(message)
+                                    response = requests.post(urlTo+message)
+                                    # print (response)
+
+                updateIds = []
+                for update in updates["result"]:
+                    updateIds.append(int(update["update_id"]))
+                lastUpdateId = max(updateIds)
+                lastUpdateId = lastUpdateId + 1
+            time.sleep(0.5)
 
 if __name__ == '__main__':
     main()
